9b54461d115cfda4a1a06b43f294ef1a101989fb
Alexandre Ribeiro Ferreira <xanderifer1974@gmail.com>
2021-01-02 00:43:08 -0300

    Criando a camada viewModel

--- lib/app/app_controller.dart
+++ lib/app/app_controller.dart
@@ -1,25 +1,17 @@
 import 'package:flutter/cupertino.dart';
-import 'package:projeto_arquitetura/app/models/appconfig_model.dart';
-import 'package:projeto_arquitetura/app/interface/local_storage_interface.dart';
+import 'package:projeto_arquitetura/app/viewmodels/change_theme_viewmodel.dart';
 import 'package:projeto_arquitetura/app/services/shared_local_storage_service.dart';
 
 class AppController {
   static final AppController instance = AppController._();
-
-  AppController._(){
-      storage.get('isDark').then((value){
-       if (value != null) config.themeSwitch.value = value;
-    });
+  AppController._() {
+    changeThemeViewModel.init();
   }
 
-  final AppConfigModel config = AppConfigModel();
-  bool get isDark => config.themeSwitch.value;
-  ValueNotifier<bool> get themeSwitch => config.themeSwitch;
+  final ChangeThemeViewModel changeThemeViewModel =
+      ChangeThemeViewModel(storage: SharedLocalStorageService());
 
-  final ILocalStorage storage = SharedLocalStorageService(); 
-
-  changeTheme(bool value) {
-    config.themeSwitch.value = value;
-    storage.put('isDark', value);
-  }
+  bool get isDark => changeThemeViewModel.config.themeSwitch.value;
+  ValueNotifier<bool> get themeSwitch =>
+      changeThemeViewModel.config.themeSwitch;
 }
